3311c7d257a0e21859fe0cfc6f634b6a5057a7fb
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-07-19 16:52:58 +0600

    feat: add css rules for main list, fixed browser resize

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,33 @@
 import Link from 'next/link';
 import Search from './Search';
+import { Z_FIXED } from 'zlib';
 
 const linkStyle = {
   marginRight: 15
 };
+const h2Style = {
+  margin: 0,
+  textAlign: 'center'
+};
+const headerStyle = {
+  position: 'fixed',
+  margin: 0,
+  padding: '15px 25px',
+  top: 0,
+  width: "100vw",
+  background: 'white',
+  zIndex: 5,
+  fontFamily: 'Arial, Tahoma'
+};
+const nextStyle = {
+  textAlign: 'center',
+  marginTop: 10
+}
+
 
 const Header = () => (
-  <div>
+  <div style={headerStyle}>
+  <h2 style={h2Style}>Unsplash photos</h2>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
@@ -14,6 +35,10 @@ const Header = () => (
       <a style={linkStyle}>About</a>
     </Link>
     <Search />
+    <div style={nextStyle}>
+      <button>Prev 30</button>
+      <button>Next 30</button>
+    </div>
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -7,29 +7,71 @@ const APIkey = 'add7029af557a934c8737dd2594d2345a4ae37d1bf154e524df5f07d9054daf2
 
 const App = props => (
   <Layout>
-    <h2>Hello Next.js</h2>
-    <div>
-      <ul className="photo-list">
-        {props.photos.map(photo => (
-          <li key={photo.id}>
-            <Link href= {`https://unsplash.com/photos/${photo.id}`}>
-              <a>{photo.user.username}</a>
-            </Link>
-            <img src = {photo.urls.small}/>
-          </li>
-        ))}
-      </ul>
+    <div className="photo-list">
+      {props.photos.map(photo => (
+        <div key={photo.id} className='photo-cart'>
+          <Link href= {`https://unsplash.com/photos/${photo.id}`}>
+            <a>{photo.user.username}</a>
+          </Link>
+          <img src = {photo.urls.small}/>
+        </div>
+      ))}
       <style jsx>{`
           .photo-list{
-            column-count: 3;
+            margin: 150px auto 0 auto;
+            width: calc(100% - 40px);
+            height: 250vw;
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            align-content: center;
+            height: 1810px;
           }
-          .photo-list > li{
-             display: inline-block;
-             margin: 10px;
-             text-align: center;
+
+          .photo-cart{
+            margin: 0;
+            width: 20%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+            position: relative;
           }
-          .photo-list > li >img{
-             display: block;
+          .photo-cart a{
+            position: absolute;
+            top: 8px;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 1;
+          }
+          .photo-cart > img{
+            object-fit: cover;
+            height: 300px;
+            position: relative;
+            z-index: 0;
+            transition: .8s opacity;
+          }
+          .photo-list:hover img{
+            opacity: 0.4;
+          }
+          .photo-list:hover img:hover{
+            opacity: 1;
+          }
+          
+          @media screen and (max-width: 499px) {
+	          .photo-list { 
+              height: 9010px; 
+              min-width: 359px;
+            }
+	          .photo-cart { width: 100%; }
+          }
+          @media screen and (min-width: 500px) and (max-width: 899px) {
+	          .photo-list { height: 4510px; }
+            .photo-cart { width: 50%; }
+          }
+          @media screen and (min-width: 900px) and (max-width: 1500px) {
+	          .photo-list { height: 3010px; }
+            .photo-cart { width: 33.3%; }
           }
         `}
       </style>
